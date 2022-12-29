a3bd2fa707bd2771eed0fba47c69389660cef8b9
Evan Voogd <voogd004@umn.edu>
2022-12-29 13:00:50 -0600

    Created project creation screen

---
+++ app/src/main/java/com/evanv/taskapp/ui/additem/ProjectEntry.java
@@ -0,0 +1,122 @@
+package com.evanv.taskapp.ui.additem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.evanv.taskapp.R;
+
+public class ProjectEntry extends AppCompatActivity {
+    // Key for the String representing the Project name.
+    private static final String EXTRA_NAME = "com.evanv.taskapp.ui.additem.Project.EXTRA_NAME";
+    // Key for the String representing the Project goal.
+    private static final String EXTRA_GOAL = "com.evanv.taskapp.ui.additem.Project.EXTRA_GOAL";
+    // Key for the int representing the color.
+    private static final String EXTRA_COLOR = "com.evanv.taskapp.ui.additem.Project.EXTRA_COLOR";
+    // Key for the bundle that contains information on the Project.
+    private static final String EXTRA_ITEM = "com.evanv.taskapp.ui.additem.Project.EXTRA_ITEM";
+    private int color; // User-selected project color
+
+    /**
+     * On creation of the project entry screen.
+     *
+     * @param savedInstanceState not used
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_entry);
+
+        color = 11;
+    }
+
+    /**
+     * Creates dialog to choose color of the project.
+     *
+     * @param view not used
+     */
+    public void handleColorPress(View view) {
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.color_picker);
+        dialog.setTitle("Pick Project Color");
+
+        ImageButton[] buttons = new ImageButton[12];
+        buttons[0] = dialog.findViewById(R.id.paleBlueButton);
+        buttons[1] = dialog.findViewById(R.id.blueButton);
+        buttons[2] = dialog.findViewById(R.id.paleGreenButton);
+        buttons[3] = dialog.findViewById(R.id.greenButton);
+        buttons[4] = dialog.findViewById(R.id.pinkButton);
+        buttons[5] = dialog.findViewById(R.id.redButton);
+        buttons[6] = dialog.findViewById(R.id.paleOrangeButton);
+        buttons[7] = dialog.findViewById(R.id.orangeButton);
+        buttons[8] = dialog.findViewById(R.id.lavenderButton);
+        buttons[9] = dialog.findViewById(R.id.purpleButton);
+        buttons[10] = dialog.findViewById(R.id.yellowButton);
+        buttons[11] = dialog.findViewById(R.id.grayButton);
+
+        for (ImageButton b : buttons) {
+            b.setSelected(false);
+
+            b.setOnClickListener(v -> {
+                for (ImageButton button : buttons) {
+                    button.setSelected(false);
+                    button.setImageDrawable(
+                            ContextCompat.getDrawable(this, R.drawable.ic_baseline_circle_24));
+                }
+
+                v.setSelected(true);
+                ((ImageButton) v).setImageDrawable(
+                        ContextCompat.getDrawable(this, R.drawable.ic_select_color_24));
+            });
+        }
+
+        buttons[11].setSelected(true);
+
+        dialog.findViewById(R.id.selectButton).setOnClickListener(view1 -> {
+            // Set color to selected color.
+            for (color = 0; color < 12; color++) {
+                if (buttons[color].isSelected()) {
+                    break;
+                }
+            }
+        });
+
+        dialog.show();
+    }
+
+    /**
+     * If no required fields are empty, pack user input into a bundle and send it.
+     */
+    public void submit() {
+        String name = String.valueOf(((EditText)findViewById(R.id.editTextProjectName)).getText());
+        String goal = String.valueOf(((EditText)findViewById(R.id.editTextGoal)).getText());
+
+        if (name.equals("")) {
+            Toast.makeText(this, R.string.name_reminder, Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (goal.equals("")) {
+            Toast.makeText(this, R.string.goal_reminder, Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        Bundle bundle = new Bundle();
+
+        bundle.putString(EXTRA_NAME, name);
+        bundle.putString(EXTRA_GOAL, goal);
+        bundle.putInt(EXTRA_COLOR, color);
+
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_ITEM, bundle);
+        setResult(RESULT_OK, replyIntent);
+        finish();
+    }
+}

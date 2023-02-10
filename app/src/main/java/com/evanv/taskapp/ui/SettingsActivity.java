e3f8d30f43adf4b71609c320bf9480b5e2aad17f
Evan Voogd <voogd004@umn.edu>
2023-02-10 16:19:50 -0600

    added backup functionality

--- app/src/main/java/com/evanv/taskapp/ui/SettingsActivity.java
+++ app/src/main/java/com/evanv/taskapp/ui/SettingsActivity.java
@@ -3,16 +3,36 @@ package com.evanv.taskapp.ui;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.compose.ui.text.android.InternalPlatformTextApi;
+import androidx.core.app.ActivityOptionsCompat;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.evanv.taskapp.R;
+import com.evanv.taskapp.db.TaskAppRoomDatabase;
+import com.evanv.taskapp.ui.main.MainActivity;
 
-public class SettingsActivity extends AppCompatActivity {
+import java.io.File;
+import java.net.URI;
+
+import de.raphaelebner.roomdatabasebackup.core.RoomBackup;
+
+@InternalPlatformTextApi public class SettingsActivity extends AppCompatActivity {
+    private RoomBackup mBackup;
+
+    private final int CODE_EXPORT = 100;
+    private final int CODE_IMPORT = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +48,39 @@ public class SettingsActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        // Create a backup system
+        mBackup = new RoomBackup(this);
+        mBackup.database(TaskAppRoomDatabase.getDatabase(this));
+
+        // Handle backup completion
+        mBackup.onCompleteListener((success, message, exitCode) -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            if (success) {
+                builder.setTitle(R.string.restore_title_success);
+                builder.setCancelable(false);
+                builder.setMessage(R.string.restore_message_success);
+                builder.setPositiveButton(R.string.ok, (d, i) -> System.exit(0));
+            }
+            else {
+                builder.setTitle(R.string.restore_title_failure);
+                builder.setMessage(R.string.restore_message_failure);
+                builder.setPositiveButton(R.string.ok, null);
+            }
+            builder.show();
+        });
+
+        // Backup the database
+        findViewById(R.id.exportButton).setOnClickListener(v -> {
+            mBackup.backupLocation(RoomBackup.BACKUP_FILE_LOCATION_CUSTOM_DIALOG);
+            mBackup.backup();
+        });
+
+        // Restore the backup
+        findViewById(R.id.importButton).setOnClickListener(v -> {
+            mBackup.backupLocation(RoomBackup.BACKUP_FILE_LOCATION_CUSTOM_DIALOG);
+            mBackup.restore();
+        });
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
